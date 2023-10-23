bc28879816108a00e38bdc90955c9dc597cc30e4
aodhanlutetiae <39188450+aodhanlutetiae@users.noreply.github.com>
2023-10-23 21:35:39 +0100

    titles

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'dj_reading'
+copyright = "2023, Aidan O'Donnell"
+author = '@aodhanlutetiae'
 
 release = '0.1'
 version = '0.1.0'
